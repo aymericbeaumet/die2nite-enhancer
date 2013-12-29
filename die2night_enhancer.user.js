a748601348ebd70fa1678d5f34e10c711a7289fb
Aymeric Beaumet <aymeric@beaumet.me>
2013-12-29 09:08:08 +0100

    Basic skeleton

---
+++ die2night_enhancer.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Die2Night Enhancer
+// @description Enhance your game experience!
+// @author Aymeric Beaumet <aymeric@beaumet.me>
+// @license zlib/libpng http://opensource.org/licenses/Zlib
+// @match *://www.die2nite.com/*
+// @match *://www.hordes.fr/*
+// @include *://www.die2nite.com/*
+// @include *://www.hordes.fr/*
+// @version 0.0.1
+// ==/UserScript==
+
+// Local scope
+;(function(){
+
+})();
