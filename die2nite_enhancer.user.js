f823f963eba173b59d688588cbb738f827f9b32a
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-04 12:00:11 +0100

    CSS rules to hide hero adds are no !important

--- die2nite_enhancer.user.js
+++ die2nite_enhancer.user.js
@@ -779,7 +779,7 @@ var D2NE = (function() {
         hide_hero_adds: function() {
             js.injectCSS(
                 '.heroMode, #ghostHeroAd, #heroContainer, .heroAd, .promoBt, .sondageBg {' +
-                'display: none;' +
+                    'display: none !important;' +
                 '}'
             );
         },
