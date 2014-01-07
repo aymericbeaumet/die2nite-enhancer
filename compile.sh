799bf29f718d1dfb39c02380c07419a4a763b1bc
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-07 16:34:43 +0100

    Compile Opera extension with script + add icons

--- compile.sh
+++ compile.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Abort on error
 set -e
 
-# Configuration
-ABSPATH=$(cd "$(dirname "$0")"; pwd)
+ABSPATH="$(cd "$(dirname "$0")"; pwd)"
+
+USERSCRIPT="$ABSPATH/userscript/die2nite_enhancer.user.js"
+ICON_DIR="$ABSPATH/icons"
 OUTPUT_DIR="$ABSPATH/build"
-BASE_USERSCRIPT="$ABSPATH/userscript/die2nite_enhancer.user.js"
+
+PEM="$HOME/.pem/die2nite_enhancer.pem"
+CHROME='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
 
 # Init the compilation
 function init()
@@ -16,19 +20,47 @@ function init()
   mkdir -p "$OUTPUT_DIR"
 }
 
-# Build the chrome extension
-function compile_chrome()
+# Build the Google Chrome extension
+function compile_chrome_ext()
 {
   chrome_source_dir="$ABSPATH/chrome"
   chrome_build_dir="$OUTPUT_DIR/chrome"
+  chrome_ext="$OUTPUT_DIR/chrome.zip"
+
+  # Gather sources
   cp -r "$chrome_source_dir" "$chrome_build_dir"
-  cp "$BASE_USERSCRIPT" "$chrome_build_dir"
-  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension="$chrome_build_dir" --pack-extension-key="$HOME/.pem/die2nite_enhancer.pem"
+  cp "$ICON_DIR"/icon{48,128}.png "$chrome_build_dir"
+  cp "$USERSCRIPT" "$chrome_build_dir"
+
+  # Create the zip and clean
+  find "$chrome_build_dir" | zip -j "$chrome_ext" -@
   rm -rf "$chrome_build_dir"
-  echo '-- Chrome extension compiled!'
+
+  echo '-- Google Chrome extension zipped!'
 }
 
+# Build the Opera extension
+function compile_opera_ext()
+{
+  opera_source_dir="$ABSPATH/opera"
+  opera_build_dir="$OUTPUT_DIR/opera"
+
+  # Gather sources
+  cp -r "$opera_source_dir" "$opera_build_dir"
+  cp "$ICON_DIR"/icon{48,128}.png "$opera_build_dir"
+  cp "$USERSCRIPT" "$opera_build_dir"
+
+  # Compile and clean
+  "$CHROME" --pack-extension="$opera_build_dir" --pack-extension-key="$PEM"
+  mv "$OUTPUT_DIR"/opera.{crx,nex}
+  rm -rf "$opera_build_dir"
+
+  echo '-- Opera extension compiled!'
+}
+
+
 init
-compile_chrome
+compile_chrome_ext
+compile_opera_ext
 
 exit 0
