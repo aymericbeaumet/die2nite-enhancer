764766220a84b4b3b8fec2ebc70d845ae0ab7b6f
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-10 14:17:28 +0100

    Firefox wrapper loads faster

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -3,5 +3,6 @@ var self = require('sdk/self');
 
 pageMod.PageMod({
   include: __MATCHING_URL_JSON_ARRAY__,
-  contentScriptFile: self.data.url("die2nite_enhancer.user.js")
+  contentScriptFile: self.data.url("die2nite_enhancer.user.js"),
+  contentScriptWhen: "start"
 });
