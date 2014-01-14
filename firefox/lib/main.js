2c6a96f8704735ed073dcbf1e2fe32411046a354
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-14 21:48:40 +0100

    Fix building script for --all

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -3,6 +3,6 @@ var self = require('sdk/self');
 
 pageMod.PageMod({
   include: __MATCHING_URL_JSON_ARRAY__,
-  contentScriptFile: self.data.url("die2nite_enhancer.user.js"),
+  contentScriptFile: self.data.url("userscript.user.js"),
   contentScriptWhen: "start"
 });
