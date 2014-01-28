03616d61d1293e77712713e36aa27bd5a0cabf0d
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-28 00:40:05 +0000

    Code factorisation in the configuration panel

--- userscript/3_modules/configuration_panel.js
+++ userscript/3_modules/configuration_panel.js
@@ -140,11 +140,124 @@ Module.register(function() {
             // Inject it into the object and save
             module.properties[property] = input_data;
             module.save_properties();
-
-            console.log('----------');
-            console.log(module);
-            console.log(input_data);
         }
+    }
+
+    /**
+     * Inject the configuration panel CSS.
+     */
+    function inject_configuration_panel_css()
+    {
+        JS.injectCSS(
+
+            '#d2ne_configuration_panel {' +
+                'z-index: 9;' +
+                'position: absolute;' +
+                'margin-top: 5px;' +
+                'margin-left: 44px;' +
+                'background-color: #5c2b20;' +
+                'border: 1px solid #000000;' +
+                'max-width: 430px;' +
+            '}' +
+
+            '#d2ne_configuration_panel h1 {' +
+                'height: auto;' +
+                'font-size: 8pt;' +
+                'text-transform: none;' +
+                'font-variant: small-caps;' +
+                'background: none;' +
+                'cursor: help;' +
+                'margin: 0;' +
+                'padding: 0;' +
+            '}' +
+            '#d2ne_configuration_panel:hover h1 {' +
+                'border-bottom: 1px solid #b37c4a;' +
+                'margin-bottom: 5px;' +
+            '}' +
+
+            '#d2ne_configuration_panel > div {' +
+                'line-height: 23px;' +
+                'border: 1px solid #f0d79e;' +
+                'padding-left: 5px;' +
+                'padding-right: 5px;' +
+            '}' +
+
+            '#d2ne_configuration_panel p {' +
+                'padding-bottom: 7px;' +
+                'margin-bottom: 3px;' +
+                'font-size: 9pt;' +
+                'line-height: 11pt;' +
+                'text-align: justify;' +
+                'border-bottom: 1px dashed #ddab76;' +
+            '}' +
+
+            '#d2ne_configuration_panel div > div > div > h4 {' +
+                'text-align: left;' +
+                'border-bottom: 1px dotted rgba(221, 171, 118, 0.8);' +
+                'padding-bottom: 4px;' +
+                'margin-bottom: 5px;' +
+                'margin-top: 4px;' +
+            '}' +
+            '#d2ne_configuration_panel div > div > div > h4 > img {' +
+                'vertical-align: -11%;' +
+                'margin-right: 5px;' +
+            '}' +
+
+            '#d2ne_configuration_panel div > div {' +
+                'position: relative;' +
+            '}' +
+            '#d2ne_configuration_panel div > div > div img {' +
+                'position: absolute;' +
+                'top: 0;' +
+                'bottom: 0;' +
+                'right: 0;' +
+                'margin: auto;' +
+                'margin-right: 4px;' +
+            '}' +
+
+            'a.d2ne_tooltip {' +
+                'display: inline;' +
+                'cursor: help' +
+            '}' +
+            'a.d2ne_tooltip img {' +
+                'border: 1px solid #5c2b20;' +
+            '}' +
+            'a.d2ne_tooltip img:hover {' +
+                'border: 1px solid #ffffff;' +
+            '}' +
+            'a.d2ne_tooltip:hover:after {' +
+                'line-height: normal;' +
+                'z-index: 98;' +
+                'position: absolute;' +
+                'top: 3px;' +
+                'right: -305px;' +
+                'content: attr(tooltip);' +
+                'font-family: Verdana;' +
+                'font-size: 12px;' +
+                'color: #ffffff;' +
+                'border: 1px solid #ecb98a;' +
+                'background-color: #5c2b20;' +
+                'background-image: url("/gfx/design/iconHelp.gif");' +
+                'background-position: 5px 0px;' +
+                'background-repeat: no-repeat;' +
+                'width: 250px;' +
+                'padding: 5px 10px 9px 30px;' +
+            '}' +
+
+            '#d2ne_configuration_panel > div > div > div:last-child {' +
+                'text-align: right;' +
+            '}' +
+
+            '#d2ne_configuration_panel a.button {' +
+                'width: auto;' +
+                'text-align: center;' +
+                'padding: 0;' +
+                'padding-top: 2px;' +
+                'height: 19px;' +
+                'margin: 0;' +
+                'margin-top: 5px;' +
+            '}'
+        );
     }
 
     /************************
@@ -172,117 +285,8 @@ Module.register(function() {
 
             load: function() {
                 JS.wait_for_id('main', function(node) {
-                    // Create and inject panel style
-                    JS.injectCSS(
-
-                        '#d2ne_configuration_panel {' +
-                            'z-index: 9;' +
-                            'position: absolute;' +
-                            'margin-top: 5px;' +
-                            'margin-left: 44px;' +
-                            'background-color: #5c2b20;' +
-                            'border: 1px solid #000000;' +
-                            'max-width: 430px;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel h1 {' +
-                            'height: auto;' +
-                            'font-size: 8pt;' +
-                            'text-transform: none;' +
-                            'font-variant: small-caps;' +
-                            'background: none;' +
-                            'cursor: help;' +
-                            'margin: 0;' +
-                            'padding: 0;' +
-                        '}' +
-                        '#d2ne_configuration_panel:hover h1 {' +
-                            'border-bottom: 1px solid #b37c4a;' +
-                            'margin-bottom: 5px;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel > div {' +
-                            'line-height: 23px;' +
-                            'border: 1px solid #f0d79e;' +
-                            'padding-left: 5px;' +
-                            'padding-right: 5px;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel p {' +
-                            'padding-bottom: 7px;' +
-                            'margin-bottom: 3px;' +
-                            'font-size: 9pt;' +
-                            'line-height: 11pt;' +
-                            'text-align: justify;' +
-                            'border-bottom: 1px dashed #ddab76;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel div > div > div > h4 {' +
-                            'text-align: left;' +
-                            'border-bottom: 1px dotted rgba(221, 171, 118, 0.8);' +
-                            'padding-bottom: 4px;' +
-                            'margin-bottom: 5px;' +
-                            'margin-top: 4px;' +
-                        '}' +
-                        '#d2ne_configuration_panel div > div > div > h4 > img {' +
-                            'vertical-align: -11%;' +
-                            'margin-right: 5px;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel div > div {' +
-                            'position: relative;' +
-                        '}' +
-                        '#d2ne_configuration_panel div > div > div img {' +
-                            'position: absolute;' +
-                            'top: 0;' +
-                            'bottom: 0;' +
-                            'right: 0;' +
-                            'margin: auto;' +
-                            'margin-right: 4px;' +
-                        '}' +
-
-                        'a.d2ne_tooltip {' +
-                            'display: inline;' +
-                            'cursor: help' +
-                        '}' +
-                        'a.d2ne_tooltip img {' +
-                            'border: 1px solid #5c2b20;' +
-                        '}' +
-                        'a.d2ne_tooltip img:hover {' +
-                            'border: 1px solid #ffffff;' +
-                        '}' +
-                        'a.d2ne_tooltip:hover:after {' +
-                            'line-height: normal;' +
-                            'z-index: 98;' +
-                            'position: absolute;' +
-                            'top: 3px;' +
-                            'right: -305px;' +
-                            'content: attr(tooltip);' +
-                            'font-family: Verdana;' +
-                            'font-size: 12px;' +
-                            'color: #ffffff;' +
-                            'border: 1px solid #ecb98a;' +
-                            'background-color: #5c2b20;' +
-                            'background-image: url("/gfx/design/iconHelp.gif");' +
-                            'background-position: 5px 0px;' +
-                            'background-repeat: no-repeat;' +
-                            'width: 250px;' +
-                            'padding: 5px 10px 9px 30px;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel > div > div > div:last-child {' +
-                            'text-align: right;' +
-                        '}' +
-
-                        '#d2ne_configuration_panel a.button {' +
-                            'width: auto;' +
-                            'text-align: center;' +
-                            'padding: 0;' +
-                            'padding-top: 2px;' +
-                            'height: 19px;' +
-                            'margin: 0;' +
-                            'margin-top: 5px;' +
-                        '}'
-                    );
+                    // Inject panel style
+                    inject_configuration_panel_css();
 
                     var config_panel_div = JS.jsonToDOM(
                         ["div", { "id": "d2ne_configuration_panel" },
