9589f451856f7329f095f65ab4bacf35ca23f457
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-28 12:21:32 +0000

    Working external tool progress bar

--- userscript/3_modules/external_tools_bar.js
+++ userscript/3_modules/external_tools_bar.js
@@ -9,21 +9,8 @@ Module.register(function() {
     /**
      * The external tools bar ID.
      */
+    var EXTERNAL_TOOLS_BAR_UPDATE_CONTAINER_ID = 'd2ne_external_tools_bar_update_container_id';
     var EXTERNAL_TOOLS_BAR_UPDATE_ID = 'd2ne_external_tools_bar_update_id';
-
-    /**
-     * External tools bar DOM nodes cache.
-     */
-    var toolbar_link_ = null;
-    var toolbar_start_ = null;
-    var toolbar_loading_ = null;
-    var toolbar_success_ = null;
-    var toolbar_failure_ = null;
-
-    /**
-     * Remember if an external tool update is currently in progress.
-     */
-    var update_in_progress_ = false;
 
     /**
      * Update state, an object containing objects of the following form:
@@ -44,6 +31,13 @@ Module.register(function() {
     var update_state_ = {};
 
     /**
+     * Cache
+     */
+    var button_container_ = null;
+    var button_hidden_div_ = null;
+    var button_link_ = null;
+
+    /**
      * Add the i18n strings for this module.
      */
     function add_i18n()
@@ -60,15 +54,65 @@ Module.register(function() {
     }
 
     /**
+     * Check if an update is in progress.
+     */
+    function is_update_in_progress()
+    {
+        return Object.keys(update_state_).length > 0;
+    }
+
+    /**
+     * Reset the update state.
+     */
+    function reset_update()
+    {
+        update_state_ = {};
+    }
+
+    /**
+     * Enable the button.
+     */
+    function enable_button()
+    {
+        button_link_.classList.remove('disabled');
+    }
+
+    /**
+     * Disable the button.
+     */
+    function disable_button()
+    {
+        button_link_.classList.add('disabled');
+    }
+
+    /**
+     * Set the size of the hidden div. Also enable/disable the button behind.
+     * @param integer updated number of updated tools (with success or not)
+     * @param integer total total number of tools
+     */
+    function update_hidden_div_width(updated, total)
+    {
+        var percent = (updated / total) * 100;
+
+        // change the div width
+        button_hidden_div_.style.width = (100 - percent) + '%';
+    }
+
+    /**
      * Update all the enabled external tools.
      * @param Function progress_callback Will be called on each state change
      */
     function update_external_tools(progress_callback)
     {
         // Abort if an update is already in progress
-        if (update_in_progress_) {
+        if (is_update_in_progress()) {
             return;
         }
+
+        // if first launch, update the hidden div width (0/100)
+        update_hidden_div_width(0, 100);
+        // and disable the button
+        disable_button();
 
         Module.iterate_on_type(Module.TYPE.EXTERNAL_TOOL, function(module) {
             if (!module.is_enabled()) {
@@ -98,83 +142,27 @@ Module.register(function() {
     }
 
     /**
-     * Show a start icon on the toolbar.
-     */
-    function show_start()
-    {
-        toolbar_start_.style.display = 'inline';
-        toolbar_loading_.style.display = 'none';
-        toolbar_success_.style.display = 'none';
-        toolbar_failure_.style.display = 'none';
-        toolbar_link_.classList.remove('off');
-    }
-
-    /**
-     * Show a loading wheel on the toolbar.
-     */
-    function show_loading_wheel()
-    {
-        toolbar_start_.style.display = 'none';
-        toolbar_loading_.style.display = 'inline';
-        toolbar_success_.style.display = 'none';
-        toolbar_failure_.style.display = 'none';
-        toolbar_link_.classList.add('off');
-    }
-
-    /**
-     * Show a success on the toolbar.
-     */
-    function show_success()
-    {
-        toolbar_start_.style.display = 'none';
-        toolbar_loading_.style.display = 'none';
-        toolbar_success_.style.display = 'inline';
-        toolbar_failure_.style.display = 'none';
-        toolbar_link_.classList.remove('off');
-    }
-
-    /**
-     * Show an error on the toolbar.
-     */
-    function show_error()
-    {
-        toolbar_start_.style.display = 'none';
-        toolbar_loading_.style.display = 'none';
-        toolbar_success_.style.display = 'none';
-        toolbar_failure_.style.display = 'inline';
-        toolbar_link_.classList.remove('off');
-    }
-
-    /**
      * Handle the click on the update button.
      */
     function on_update_button_click()
     {
-        update_in_progress_ = true;
-        show_loading_wheel();
-
         update_external_tools(function on_progress() {
             var number_of_tools = 0;
             var number_of_done = 0;
-            var number_of_error = 0;
 
             for (var key in update_state_) {
                 ++number_of_tools;
                 number_of_done += (update_state_[key].done === true) ? 1 : 0;
-                number_of_error += (update_state_[key].error === true) ? 1 : 0;
             }
+
+            update_hidden_div_width(number_of_done, number_of_tools);
 
             // if done
             if (number_of_done >= number_of_tools) {
-                update_in_progress_ = false;
-                update_state_ = {};
-                // if error
-                if (number_of_error > 0) {
-                    show_error();
-                // if success
-                } else {
-                    show_success();
-                }
+                // finally reset the update
+                reset_update();
+                // enable the button
+                enable_button();
             }
         });
     }
@@ -206,22 +194,26 @@ Module.register(function() {
 
             // Create the new node
             var new_button = JS.jsonToDOM(
-                ["a", { "href": "javascript:void(0)", "class": "button", "id": EXTERNAL_TOOLS_BAR_UPDATE_ID,
-                    "onclick": function() { on_update_button_click(); } },
-                    ["img", { "src": "/gfx/icons/r_explo2.gif", "width": "16px", "height": "16px" }],
-                    ["img", { "src": "/gfx/design/loading.gif", "width": "16px", "height": "16px", "style": "display: none;" }],
-                    ["img", { "src": "/gfx/icons/small_nice_lock.gif", "width": "16px", "height": "16px", "style": "display: none;" }],
-                    ["img", { "src": "/gfx/icons/item_radius_mk2_part.gif", "width": "16px", "height": "16px", "style": "display: none;" }],
-                    ' ' + I18N.get(MODULE_NAME + '_update_button')
+                ["div", { "id": EXTERNAL_TOOLS_BAR_UPDATE_CONTAINER_ID },
+                    ["div", {}],
+                    ["a", { "href": "javascript:void(0)", "class": "button", "id": EXTERNAL_TOOLS_BAR_UPDATE_ID,
+                            "onclick": function() {
+                                           if (this.classList.contains('disabled')) {
+                                               return;
+                                           }
+                                           on_update_button_click();
+                                       }
+                          },
+                        ["img", { "src": "/gfx/icons/r_explo2.gif", "width": "16px", "height": "16px" }],
+                        ' ' + I18N.get(MODULE_NAME + '_update_button')
+                    ]
                 ]
             , document);
 
-            // Cache the nodes
-            toolbar_link_ = new_button;
-            toolbar_start_ = new_button.childNodes[0];
-            toolbar_loading_ = new_button.childNodes[1];
-            toolbar_success_ = new_button.childNodes[2];
-            toolbar_failure_ = new_button.childNodes[3];
+            // Cache it
+            button_container_ = new_button;
+            button_hidden_div_ = new_button.childNodes[0];
+            button_link_ = new_button.childNodes[1];
 
             // Insert it
             JS.insert_after(reference_node, new_button);
@@ -231,6 +223,42 @@ Module.register(function() {
                 inject_external_tools_bar_nodes();
             }, false);
         });
+    }
+
+    /**
+     * Inject the external tool bar CSS.
+     */
+    function inject_external_tools_bar_style()
+    {
+        JS.injectCSS(
+            '#' + EXTERNAL_TOOLS_BAR_UPDATE_CONTAINER_ID + ' {' +
+                'position: relative;' +
+                'top: 0;' +
+                'left: 0;' +
+            '}' +
+
+            '#' + EXTERNAL_TOOLS_BAR_UPDATE_CONTAINER_ID + ' div {' +
+                'position: absolute;' +
+                'top: 0;' +
+                'right: 0;' +
+                'width: 0%;' +
+                'height: 100%;' +
+                'background-color: black;' +
+                'opacity: 0.5;' +
+            '}' +
+
+            '#' + EXTERNAL_TOOLS_BAR_UPDATE_CONTAINER_ID + ' a.disabled {' +
+                'padding-top: 0px;' +
+                'padding-bottom: 1px;' +
+                'color: #f0d79e;' +
+                'border-top-width: 1px;' +
+                'border-bottom-width: 2px;' +
+                'outline: 1px solid #784323;' +
+                'color: #f0d79e;' +
+                'cursor: help;' +
+            '}'
+
+        );
     }
 
 
@@ -257,6 +285,7 @@ Module.register(function() {
             },
 
             load: function() {
+                inject_external_tools_bar_style();
                 inject_external_tools_bar_nodes();
             }
         }
