9085b72e0ca3e5e670b3297ed0dcaf6d57976eb8
Aymeric Beaumet <aymeric@beaumet.me>
2014-01-31 23:19:09 +0000

    Add tests for the JS::is_defined method

---
+++ tests/classes/JS.tests.js
@@ -0,0 +1,32 @@
+describe("network_request", function() {
+});
+
+describe("is_defined", function() {
+    it("should return false if undefined", function() {
+        expect(JS.is_defined(undefined)).toBe(false);
+    });
+
+    it("should return false if null", function() {
+        expect(JS.is_defined(null)).toBe(false);
+    });
+
+    it("should return true if integer 0", function() {
+        expect(JS.is_defined(0)).toBe(true);
+    });
+
+    it("should return true if empty string", function() {
+        expect(JS.is_defined("")).toBe(true);
+    });
+
+    it("should return true if empty array", function() {
+        expect(JS.is_defined([])).toBe(true);
+    });
+
+    it("should return true if empty object", function() {
+        expect(JS.is_defined({})).toBe(true);
+    });
+
+    it("should return true if close", function() {
+        expect(JS.is_defined(function(){})).toBe(true);
+    });
+});
--- tests/karma.config.js
+++ tests/karma.config.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../build/compiled.js',
+      '../sources/classes/*.js',
       '**/*.tests.js'
     ],
 
