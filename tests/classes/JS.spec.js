17200d16bc8a62dc3ed9383d8191ccccbf5b36bb
Aymeric Beaumet <aymeric@beaumet.me>
2014-02-02 15:55:23 +0000

    JS.is_defined unit tests are more precise

--- tests/classes/JS.spec.js
+++ tests/classes/JS.spec.js
@@ -2,31 +2,31 @@ describe("JS.network_request", function() {
 });
 
 describe("JS.is_defined", function() {
-    it("should return false if undefined", function() {
+    it("should return false if the parameter undefined", function() {
         expect(JS.is_defined(undefined)).toBe(false);
     });
 
-    it("should return false if null", function() {
+    it("should return false if the parameter null", function() {
         expect(JS.is_defined(null)).toBe(false);
     });
 
-    it("should return true if integer 0", function() {
+    it("should return true if the parameter integer 0", function() {
         expect(JS.is_defined(0)).toBe(true);
     });
 
-    it("should return true if empty string", function() {
+    it("should return true if the parameter empty string", function() {
         expect(JS.is_defined("")).toBe(true);
     });
 
-    it("should return true if empty array", function() {
+    it("should return true if the parameter empty array", function() {
         expect(JS.is_defined([])).toBe(true);
     });
 
-    it("should return true if empty object", function() {
+    it("should return true if the parameter empty object", function() {
         expect(JS.is_defined({})).toBe(true);
     });
 
-    it("should return true if closure", function() {
+    it("should return true if the parameter closure", function() {
         expect(JS.is_defined(function(){})).toBe(true);
     });
 });
