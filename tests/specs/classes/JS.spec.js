d476d16d0d037d380921158ffea096b053d5b61e
Aymeric Beaumet <aymeric@beaumet.me>
2014-02-02 23:43:55 +0000

    Add the JS.network_request unit tests

--- tests/specs/classes/JS.spec.js
+++ tests/specs/classes/JS.spec.js
@@ -3,7 +3,93 @@
     "use strict";
 
     describe("JS.network_request", function() {
+        var url = window.location.protocol + "//" + window.location.host;
+        var urn = "/base/tests/bootstrap.js"; // random file, the test will be done based on its content
+        var bad_urn = "/this/is/leading/to/nowhere";
+        var request_timeout = 500; // ms
+        var callback_success, callback_failure;
+
+        var reference_content = $.ajax({
+            type: "GET",
+            url: urn,
+            async: false
+        }).responseText;
+
+        /**
+         * Used to perform network request.
+         * @param string method GET, POST, etc...
+         * @param string urn Where do the request should be performed?
+         * @param boolean expect_success Should is it supposed to success?
+         */
+        var test_network_request = function(method, urn, expect_success) {
+            var answer = false;
+
+            runs(function() {
+                JS.network_request(method, urn, "", {},
+                   function(content) {
+                       answer = true;
+                       callback_success(content);
+                   },
+                   function() {
+                       answer = true;
+                       callback_failure();
+                   }
+                );
+            });
+
+            waitsFor(function() {
+                return answer === true;
+            }, "The network request took too much time", request_timeout);
+
+            runs(function() {
+                if (expect_success) {
+                    expect(callback_success).toHaveBeenCalledWith(reference_content);
+                    expect(callback_failure).not.toHaveBeenCalled();
+                } else {
+                    expect(callback_success).not.toHaveBeenCalled();
+                    expect(callback_failure).toHaveBeenCalled();
+                }
+            });
+        };
+
+        beforeEach(function() {
+            callback_success = jasmine.createSpy("callback_success");
+            callback_failure = jasmine.createSpy("callback_failure");
+        });
+
+        afterEach(function() {
+            callback_success = null;
+            callback_failure = null;
+        });
+
+        describe("[XMLHttpRequest()]", function() {
+            describe("should succeed on a valid", function() {
+                describe("GET request", function() {
+                    it("(URN)", function() {
+                        test_network_request("GET", urn, true);
+                    });
+
+                    it("(URL + URN)", function() {
+                        test_network_request("GET", url + urn, true);
+                    });
+                });
+            });
+
+            describe("should fail on a non-valid", function() {
+                describe("GET request", function() {
+                    it("(URN)", function() {
+                        test_network_request("GET", bad_urn, false);
+                    });
+
+                    it("(URL + URN)", function() {
+                        test_network_request("GET", url + bad_urn, false);
+                    });
+                });
+            });
+        });
     });
+
+
 
     describe("JS.is_defined", function() {
         describe("should return false if", function() {
@@ -47,6 +133,8 @@
         });
     });
 
+
+
     describe("JS.keydown_event", function() {
         var callback;
         var node = document;
@@ -59,7 +147,8 @@
         });
 
         afterEach(function() {
-            node.removeEventListener(callback);
+            callback = null;
+            node.removeEventListener(callback); // remove the callback added by the function
         });
 
         describe("should have called the callback with", function() {
