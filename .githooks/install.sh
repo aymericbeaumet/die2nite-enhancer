f20e8a777d410c80b307b013cb4d58acc81a83b6
Aymeric Beaumet <aymeric@beaumet.me>
2014-02-05 03:53:30 +0000

    The hooks install script is more reliable

--- .githooks/install.sh
+++ .githooks/install.sh
@@ -3,10 +3,12 @@
 # exit in case of error
 set -e
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 HOOK_EXTENSION='hook'
 
-for hook in .githooks/*".$HOOK_EXTENSION" ; do
-  ln -sf "../../$hook" ".git/hooks/$(basename $hook ".$HOOK_EXTENSION")"
+for hook in *".$HOOK_EXTENSION" ; do
+  ln -sf "../../.githooks/$hook" "../.git/hooks/$(basename "$hook" ".$HOOK_EXTENSION")"
 done
 
 exit 0
