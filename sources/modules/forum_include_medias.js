df15dfc63ad70501e775b144d1af6a6079da209d
Aymeric Beaumet <aymeric@beaumet.me>
2014-03-18 15:50:58 +0100

    Enable Vimeo support

--- sources/modules/forum_include_medias.js
+++ sources/modules/forum_include_medias.js
@@ -18,18 +18,18 @@ Module.register(function() {
 
         i18n[I18N.LANG.EN] = {};
         i18n[I18N.LANG.EN][MODULE_NAME + '_short_desc'] = 'Replace the medias link by the content';
-        i18n[I18N.LANG.EN][MODULE_NAME + '_full_desc'] = 'In the forum posts, replace the media links by the concerned media (images and YouTube videos).';
+        i18n[I18N.LANG.EN][MODULE_NAME + '_full_desc'] = 'In the forum posts, replace the media links by the concerned media (images and YouTube, Vimeo and Dailymotion videos).';
 
         i18n[I18N.LANG.FR] = {};
         i18n[I18N.LANG.FR][MODULE_NAME + '_short_desc'] = 'Remplacer les liens de médias par le contenu';
-        i18n[I18N.LANG.FR][MODULE_NAME + '_full_desc'] = 'Dans les posts du forum, remplace les liens de média par le média concerné (e.g.: images, vidéos YouTube).';
+        i18n[I18N.LANG.FR][MODULE_NAME + '_full_desc'] = 'Dans les posts du forum, remplace les liens de média par le média concerné (images et vidéos YouTube, Vimeo et Dailymotion).';
 
         I18N.set(i18n);
     }
 
     /**
      * Return the id from a dailymotion url.
-     * @link http://stackoverflow.com/questions/12387389/how-to-parse-dailymotion-video-url-in-javascript
+     * @link http://stackoverflow.com/a/15942126/1071486
      * @param string video_url
      * @return integer the video id
      * @return null if can not extract it
@@ -42,6 +42,23 @@ Module.register(function() {
             return null;
         }
         return results[2] || results[1];
+    }
+
+    /**
+     * Return the id from a vimeo url.
+     * @link http://stackoverflow.com/a/16841070/1071486
+     * @param string video_url
+     * @return integer the video id
+     * @return null if can not extract it
+     */
+    function get_vimeo_video_id(video_url)
+    {
+        var results = video_url.match(/(?:https?:\/\/)?(?:www.)?(?:player.)?vimeo.com\/(?:[a-z]*\/)*([0-9]{6,11})/);
+
+        if (!results || results.length < 2) {
+            return null;
+        }
+        return results[1];
     }
 
     /**
@@ -100,17 +117,22 @@ Module.register(function() {
                 // images
                 if (is_image_link(link.href)) { // if an image
                     // create the node img
-                    new_node = JS.jsonToDOM(['img', { src: link.href, class: 'd2ne_injected' }], document);
+                    new_node = JS.jsonToDOM(['img', { class: 'd2ne_injected', src: link.href }], document);
 
                 // YouTube
                 } else if ((id = get_youtube_video_id(link.href)) !== null) {
                     embed_link = '//www.youtube-nocookie.com/embed/' + id + '?rel=0';
-                    new_node = JS.jsonToDOM(['iframe', { width: VIDEO_WIDTH, height: VIDEO_HEIGHT, src: embed_link, frameborder: 0, allowfullscreen: '', class: 'd2ne_injected' }], document);
+                    new_node = JS.jsonToDOM(['iframe', { class: 'd2ne_injected', width: VIDEO_WIDTH, height: VIDEO_HEIGHT, src: embed_link, frameborder: 0, webkitallowfullscreen: '', mozallowfullscreen: '', allowfullscreen: '' }], document);
+
+                // Vimeo
+                } else if ((id = get_vimeo_video_id(link.href)) !== null) {
+                    embed_link = '//player.vimeo.com/video/' + id;
+                    new_node = JS.jsonToDOM(['iframe', { class: 'd2ne_injected', width: VIDEO_WIDTH, height: VIDEO_HEIGHT, src: embed_link, frameborder: 0, webkitallowfullscreen: '', mozallowfullscreen: '', allowfullscreen: '' }], document);
 
                 // Dailymotion
                 } else if ((id = get_dailymotion_video_id(link.href)) !== null) {
                     embed_link = '//www.dailymotion.com/embed/video/' + id;
-                    new_node = JS.jsonToDOM(['iframe', { width: VIDEO_WIDTH, height: VIDEO_HEIGHT, src: embed_link, frameborder: 0, logo: 0, related: 0, class: 'd2ne_injected' }], document);
+                    new_node = JS.jsonToDOM(['iframe', { class: 'd2ne_injected', width: VIDEO_WIDTH, height: VIDEO_HEIGHT, src: embed_link, frameborder: 0, webkitallowfullscreen: '', mozallowfullscreen: '', allowfullscreen: '', related: 0 }], document);
 
                 // abort for this link
                 } else {
